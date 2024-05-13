e6b056cd9f46cf7580ddb3eceefe8e2232ebbf2d
woker21 <hernandezrichard674@gmail.com>
2024-05-13 23:40:16 +0200

    react native funciona

--- welcome/app/(tabs)/_layout.tsx
+++ welcome/app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Pagina 1',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -50,7 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: 'Pagina 2',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />

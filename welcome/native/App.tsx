7aaeaa94e02442a40d921b6e1b190481ab10ad7c
woker21 <hernandezrichard674@gmail.com>
2024-05-14 12:00:12 +0200

    add counter

--- welcome/native/App.tsx
+++ welcome/native/App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Pressable, Button } from 'react-native';
 
 export default function App() {
+
+  const [valor, setValor] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.white}>Hola perros</Text>
+
+      <Pressable onPress={() => setValor(valor + 1)}>
+        <Text style={[styles.white, styles.boton]}>Subir valor</Text>
+      </Pressable>
+
+      <StatusBar />
+      <Text style={styles.white}>{valor}</Text>
+      
+      <Pressable onPress={() => setValor(valor - 1)}>
+        <Text style={[styles.white, styles.botonBlue]}>Bajar valor</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +27,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0e0e0e',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
   },
+  white: {
+    color: '#fff',
+  },
+  boton: {
+    margin: 50,
+    backgroundColor: '#ff0404',
+    padding: 20,
+    borderRadius: 20,
+  },
+  botonBlue: {
+    margin: 50,
+    backgroundColor: '#0628be',
+    padding: 20,
+    borderRadius: 20,
+  }
 });
---
+++ welcome/native/app/styles/main.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+
+
+
+export const VerticalLayout = styled.View`
+
+    flex-direction: column;
+`;
